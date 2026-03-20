69df35d4dc18212d76114e969444924403ec2e4c
saha-7op <saha7pritam@gmail.com>
2026-03-20 12:40:36 +0530

    dependency clean up

--- dist/middleware/notFoundHandler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.notFoundHandler = notFoundHandler;
-function notFoundHandler(req, res) {
-    res.status(404).json({
-        error: 'NotFound',
-        message: `No route matches ${req.method} ${req.path}`
-    });
-}
